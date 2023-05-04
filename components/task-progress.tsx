ddc94bb40673dab8134df8d0eca4cdd35b63630f
MrGoropeza <goropeza8@gmail.com>
2023-05-03 23:48:12 -0300

    feat: task-progress component initial layout

---
+++ components/task-progress.tsx
@@ -0,0 +1,33 @@
+import { PropsWithoutRef } from "react";
+
+export default function TasksProgress({
+  progress,
+  title,
+}: PropsWithoutRef<{ progress: number; title: string }>) {
+  const percentage = progressToPercentage(progress);
+
+  return (
+    <div className="mx-10 my-4 rounded-xl bg-gradient-to-r from-blue-400 to-blue-700 p-4">
+      <p className="mb-2 text-2xl font-bold">{title}</p>
+
+      <div className="flex justify-between">
+        <p>Progreso</p>
+        <p>{percentage}%</p>
+      </div>
+
+      <div className="relative">
+        <div className="h-2 w-full rounded-lg bg-slate-200/50"></div>
+        <div
+          className="absolute top-0 h-2 rounded-lg bg-slate-200"
+          style={{ width: `${percentage}%` }}
+        ></div>
+      </div>
+    </div>
+  );
+}
+
+const progressToPercentage = (progress: number) => {
+  if (progress > 1) return 100;
+  if (progress < 0) return 0;
+  return progress * 100;
+};
