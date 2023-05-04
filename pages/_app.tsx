1afea269190153b997d3dcf39af618b55d0730a6
MrGoropeza <goropeza8@gmail.com>
2023-05-04 08:45:27 -0300

    feat: added ripple effect to primereact

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,9 @@ import "primereact/resources/primereact.min.css";
 import "primereact/resources/themes/md-dark-indigo/theme.css";
 
 import type { AppProps } from "next/app";
+import PrimeReact from "primereact/api";
 
 export default function App({ Component, pageProps }: AppProps) {
+  PrimeReact.ripple = true;
   return <Component {...pageProps} />;
 }
