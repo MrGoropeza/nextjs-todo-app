7207c6cf983dfc8ee012d72ce293a1ebe8c80583
MrGoropeza <goropeza8@gmail.com>
2023-05-09 11:24:18 -0300

    feat: task form with title & description inputs

--- app/home/components/TaskForm.tsx
+++ app/home/components/TaskForm.tsx
@@ -1,20 +1,58 @@
-import { InputText } from "primereact/inputtext";
+"use client";
+
+import { FormInputText } from "@/app/components";
+import { Button } from "primereact/button";
+import { Controller, SubmitHandler, useForm } from "react-hook-form";
+
+type Task = {
+  title: string;
+  description: string;
+};
 
 const TaskForm = () => {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Task>();
+  const onSubmit: SubmitHandler<Task> = (data) => {
+    console.log(data);
+  };
+
   return (
-    <div className="flex flex-col gap-2">
-      <span className="p-float-label mt-6">
-        <InputText
-          id="title"
-          aria-describedby="title-help"
-          className="p-invalid"
-        />
-        <label htmlFor="title">Título</label>
-      </span>
-      <small id="title-help" className="text-red-500">
-        Título requerido.
-      </small>
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Controller
+        name="title"
+        control={control}
+        rules={{ required: true, minLength: 5 }}
+        render={({ field, fieldState }) => (
+          <FormInputText
+            label="Título"
+            placeholder="Título de la tarea"
+            field={field}
+            fieldState={fieldState}
+          />
+        )}
+      ></Controller>
+
+      <Controller
+        name="description"
+        control={control}
+        rules={{ required: true, min: 0 }}
+        render={({ field, fieldState }) => (
+          <FormInputText
+            label="Descripción"
+            placeholder="Desc. de la tarea"
+            field={field}
+            fieldState={fieldState}
+          />
+        )}
+      ></Controller>
+
+      <div className="mt-2 flex justify-end">
+        <Button type="submit" label="Crear" />
+      </div>
+    </form>
   );
 };
 
