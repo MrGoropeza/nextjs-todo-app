b24ac5327a758e9b850b0ae30f7ec5026ad0ea56
MrGoropeza <goropeza8@gmail.com>
2023-08-19 17:42:42 -0300

    feat: cleaned project

--- app/components/forms/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as FormControlError } from "./FormControlError";
-export { default as FormFloatLabel } from "./FormFloatLabel";
-export { default as FormInputText } from "./FormInputText";
--- model/task.model.ts
+++
@@ -1,5 +0,0 @@
-export interface TaskModel {
-  id: string;
-  title: string;
-  description: string;
-}
