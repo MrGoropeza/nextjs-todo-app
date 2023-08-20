1530f4358302ae78c91b774247b8805349f01365
MrGoropeza <goropeza8@gmail.com>
2023-08-20 18:16:49 -0300

    feat: session context

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -1,8 +1,13 @@
+import SessionContextProvider from "@context/session.context";
 import { ReactNode } from "react";
 import ThemeContextProvider from "../context/theme.context";
 
 const PagesLayout = ({ children }: { children: ReactNode }) => {
-  return <ThemeContextProvider>{children}</ThemeContextProvider>;
+  return (
+    <ThemeContextProvider>
+      <SessionContextProvider>{children}</SessionContextProvider>
+    </ThemeContextProvider>
+  );
 };
 
 export default PagesLayout;
---
+++ src/app/context/session.context.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import pb from "@lib/consts/pocketbase.const";
+import { PocketBaseRealtimeAction } from "@lib/enums/pocketbase-realtime.enum";
+import { Admin, Record } from "pocketbase";
+import { ReactNode, createContext, useEffect, useState } from "react";
+
+export const SessionContext = createContext<Record | Admin | null>(null);
+
+type Props = {
+  children?: ReactNode;
+};
+
+const SessionContextProvider = ({ children }: Props) => {
+  const [user, setUser] = useState<Record | Admin | null>(null);
+
+  useEffect(() => {
+    if (user?.id) {
+      pb.collection("users").subscribe(user.id, (e) => {
+        // console.log("realtime user:", e);
+        if (e.action === PocketBaseRealtimeAction.Update)
+          pb.authStore.save(pb.authStore.token, e.record);
+      });
+    }
+
+    return () => {
+      pb.realtime.unsubscribe();
+    };
+  }, [user]);
+
+  useEffect(() => {
+    pb.collection("users")
+      .authRefresh({}, { $autoCancel: false })
+      .catch(() => pb.authStore.clear());
+
+    const removeAuthSub = pb.authStore.onChange((_, record) => {
+      if (!record) return;
+
+      record.avatar = record.avatar
+        ? pb.files.getUrl(record as Record, record.avatar)
+        : "";
+
+      setUser(record);
+    }, true);
+
+    return removeAuthSub;
+  }, []);
+
+  return (
+    <SessionContext.Provider value={user}>{children}</SessionContext.Provider>
+  );
+};
+
+export default SessionContextProvider;
