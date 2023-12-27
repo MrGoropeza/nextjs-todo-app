719f85f70000921190a52a2ee0ced994b16afb5f
MrGoropeza <goropeza8@gmail.com>
2023-12-27 18:06:57 -0300

    chore: created new next 14 app

--- app/home/components/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as RootTabMenu } from "./RootTabMenu";
-export { default as TaskForm } from "./TaskForm";
-export { default as TaskList } from "./TaskList";
-export { default as TaskModal } from "./TaskModal";
-export { default as TaskTile } from "./TaskTile";
-export { default as TasksProgress } from "./TasksProgress";
